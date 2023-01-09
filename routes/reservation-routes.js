3de6a32fb39554b254e303fb07c2a9a923c6de5c
Saifweb <seifeddine.benhamida@supcom.tn>
2023-01-09 23:42:19 +0100

    push

--- routes/reservation-routes.js
+++ routes/reservation-routes.js
@@ -5,7 +5,7 @@ const { Create, ApproveState, RejectState, getAllReservations, giverate, StateDo
 const router = express.Router();
 
 const { check } = require("express-validator")
-
+//Auth Route
 router.post('/reservation/:housekeeper_id', [check('type').exists().withMessage("type is required!"), check('start_date').exists().withMessage("start_date is required!"), check('end_date').exists().withMessage("end_date is required!")], Create);
 router.put('/reservation/approve/:id', ApproveState);
 router.put('/reservation/reject/:id', RejectState);
